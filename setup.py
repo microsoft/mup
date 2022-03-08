0518b39e91caa5d81ad0529225046cbdfdef69c4
Greg Yang <gregyang@microsoft.com>
2022-03-08 06:50:46 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mup",
+    version="1.0.0",
+    author="Edward J Hu, Greg Yang",
+    author_email="edwardjhu@edwardjhu.com, gregyang@microsoft.com",
+    description="Maximal Update Parametrization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microsoft/mup",
+    install_requires=[
+        'numpy',
+        'pandas',
+        'torch',
+        'torchvision',
+        'seaborn',
+        'tqdm',
+        'pyyaml'
+      ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
